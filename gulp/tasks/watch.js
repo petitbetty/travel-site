7cb79fcd1564a6d2acf2e57a1d39731ec9d68985
Betty <betty@lytana.net>
2017-02-16 14:19:53 +0100

    Completed desktop and mobile style

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -9,15 +9,26 @@ gulp.task('watch', function(){
             baseDir: 'app'
         }
     });
+
     watch('./app/index.html', function() {
         browserSync.reload();
     });
+
     watch('./app/assets/styles/**/*.css', function() {
         gulp.start('cssInject');
-    })
+    });
+
+    watch('./app/assets/scripts/**/*.js', function() {
+        gulp.start('scriptsRefresh');
+    });
+
 });
 
 gulp.task('cssInject',['styles'], function(){
     return gulp.src('./app/temp/styles/style.css')
     .pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function() {
+    browserSync.reload();
 });
